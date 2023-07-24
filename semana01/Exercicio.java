5070e7dbd5900f6b5badf22e32ebaa480b5e3d53
Icaro <iba1987@gmail.com>
2023-07-24 18:36:02 -0300

    exercicio 5

--- semana01/Exercicio.java
+++ semana01/Exercicio.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Exercicio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
         while (true) {
             String entry = sc.nextLine();
             if (entry.equalsIgnoreCase("fim"))
                 break;
+            names.add(entry);
         }
         sc.close();
     }
