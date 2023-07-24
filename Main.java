e67129da1cbadcb3f64e09d01d0ece08bd6b976a
Icaro <iba1987@gmail.com>
2023-07-24 20:02:07 -0300

    exercicio 7

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 import entidades.Produto;
 
 public class Main {
+    private static ArrayList<Produto> estoque = new ArrayList<>();
+
     public static void main(String[] args) {
-        Produto produto = new Produto("Produto 1", 25.0, 3);
+        estoque.add(new Produto("Produto 1", 25.0, 3));
+        estoque.add(new Produto("Produto 2", 65.0, 10));
+        estoque.add(new Produto("Produto 3", 90.0, 2));
     }
 
 }
