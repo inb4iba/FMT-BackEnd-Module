0e2db8b61092c8443fb092ec71fff86b32472da7
Icaro <iba1987@gmail.com>
2023-07-24 18:33:21 -0300

    exercicio 4

--- semana01/Exercicio.java
+++ semana01/Exercicio.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Exercicio {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String entry = sc.nextLine();
+            if (entry.equalsIgnoreCase("fim"))
+                break;
+        }
+        sc.close();
     }
 }
