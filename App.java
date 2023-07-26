326ac9c9aa37e321a67cbcf816ef727590f775ca
Icaro <iba1987@gmail.com>
2023-07-26 20:03:25 -0300

    exercicios 8 e 9

--- App.java
+++ App.java
@@ -4,26 +4,92 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import entidades.Reserva;
+import exececoes.ExcecaoDominio;
 
 public class App {
     private static Scanner scanner = new Scanner(System.in);
     private static ArrayList<Reserva> reservas = new ArrayList<>();
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static void main(String[] args) {
-        fazerReserva();
+        int entrada = -1;
+        do {
+            System.out.println("\nMENU\n");
+            System.out.println("1. Fazer uma nova reserva");
+            System.out.println("2. Alterar uma reserva");
+            System.out.println("3. Ver todas reservas");
+            System.out.println("\n0. Fechar programa");
+            try {
+                System.out.println("\nEscolha uma opção:");
+                entrada = Integer.parseInt(scanner.nextLine());
+
+                switch (entrada) {
+                    case 1:
+                        fazerReserva();
+                        break;
+                    case 2:
+                        alterarReserva();
+                        break;
+                    case 3:
+                        mostrarReservas();
+                        break;
+                    case 0:
+                        System.out.println("\nEncerrando o programa.");
+                        break;
+                    default:
+                        System.out.println("\nEssa não é uma opção válida");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("\nEntrada precisa ser o número da opção selecionada");
+                continue;
+            }
+        } while (entrada != 0);
     }
 
     private static void fazerReserva() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.println("\nNúmero do quarto:");
-        String quarto = scanner.nextLine();
-        System.out.println("Data de entrada (dd/MM/yyyy):");
-        String dtEntrada = scanner.nextLine();
-        System.out.println("Data de saída (dd/MM/yyyy):");
-        String dtSaida = scanner.nextLine();
-        Reserva reserva = new Reserva(quarto, LocalDate.parse(dtEntrada, formatter),
-                LocalDate.parse(dtSaida, formatter));
-        reservas.add(reserva);
-        System.out.println("\nReserva adicionada com sucesso");
+        String quarto = fazerPergunta("\nNúmero do quarto:");
+        String dtEntrada = fazerPergunta("Data de entrada (dd/MM/yyyy):");
+        String dtSaida = fazerPergunta("Data de saída (dd/MM/yyyy):");
+        try {
+            Reserva reserva = new Reserva(quarto, LocalDate.parse(dtEntrada, formatter),
+                    LocalDate.parse(dtSaida, formatter));
+            reservas.add(reserva);
+            System.out.println("\nReserva adicionada com sucesso");
+            System.out.println(reserva);
+        } catch (ExcecaoDominio e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void alterarReserva() {
+        try {
+            int numeroReserva = Integer.parseInt(fazerPergunta("\nQual o número da reserva:"));
+            String dtEntrada = fazerPergunta("Data de entrada (dd/MM/yyyy):");
+            String dtSaida = fazerPergunta("Data de saída (dd/MM/yyyy):");
+            Reserva reserva = reservas.get(numeroReserva);
+            reserva.atualizarReserva(LocalDate.parse(dtEntrada, formatter),
+                    LocalDate.parse(dtSaida, formatter));
+            System.out.println("\nReserva atualizada com sucesso");
+            System.out.println(reserva);
+        } catch (NumberFormatException e) {
+            System.out.println("\nNúmero inválido. Veja as reservas pelo menu.");
+        } catch (ExcecaoDominio e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static String fazerPergunta(String msg) {
+        System.out.println(msg);
+        return scanner.nextLine();
+    }
+
+    private static void mostrarReservas() {
+        reservas.forEach(r -> {
+            System.out.println(reservas.indexOf(r) + ". " + r);
+        });
     }
 }
--- entidades/Reserva.java
+++ entidades/Reserva.java
@@ -1,16 +1,19 @@
 package entidades;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import exececoes.ExcecaoDominio;
 
 public class Reserva {
     private String numeroQuarto;
     private LocalDate dtEntrada;
     private LocalDate dtSaida;
 
-    public Reserva(String numeroQuarto, LocalDate dtEntrada, LocalDate dtSaida) {
+    public Reserva(String numeroQuarto, LocalDate dtEntrada, LocalDate dtSaida) throws ExcecaoDominio {
         this.numeroQuarto = numeroQuarto;
-        this.dtEntrada = dtEntrada;
-        this.dtSaida = dtSaida;
+        checarDatas(dtEntrada, dtSaida);
     }
 
     public String getNumeroQuarto() {
@@ -35,5 +38,30 @@ public class Reserva {
 
     public void setDtSaida(LocalDate dtSaida) {
         this.dtSaida = dtSaida;
+    }
+
+    public void checarDatas(LocalDate entrada, LocalDate saida) throws ExcecaoDominio {
+        if (entrada.isAfter(saida))
+            throw new ExcecaoDominio("Erro na Reserva: Data de Saída deve ser após Data de Entrada.");
+
+        if (entrada.isBefore(LocalDate.now()))
+            throw new ExcecaoDominio("Erro na Reserva: Datas da Reserva deve ser posterior a hoje.");
+
+        this.dtEntrada = entrada;
+        this.dtSaida = saida;
+    }
+
+    public void atualizarReserva(LocalDate entrada, LocalDate saida) throws ExcecaoDominio {
+        if (entrada.isBefore(dtEntrada))
+            throw new ExcecaoDominio("Erro na Reserva: Datas da Reserva deve ser atualizada para datas futuras.");
+
+        checarDatas(entrada, saida);
+    }
+
+    public String toString() {
+        return "Reserva: Quarto " + numeroQuarto +
+                ", Entrada: " + dtEntrada.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                ", Saída: " + dtSaida.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) +
+                ", " + ChronoUnit.DAYS.between(dtEntrada, dtSaida) + " noites";
     }
 }
