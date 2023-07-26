e42cec9085c4d24c3576041c8b0461d01f6abc94
Icaro <iba1987@gmail.com>
2023-07-26 14:52:44 -0300

    exercicio 5

--- App.java
+++ App.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+
+import entidades.Produto;
+
 public class App {
+    private static ArrayList<Produto> produtos = new ArrayList<>();
+
     public static void main(String[] args) {
 
     }
