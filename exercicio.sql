790acc9668c2fd267e77eb36afcb97331ac3b71f
Icaro <iba1987@gmail.com>
2023-07-28 15:13:14 -0300

    exercicio 4

--- exercicio.sql
+++ exercicio.sql
@@ -1,7 +1,17 @@
 -- BANCO UTILIZADO POSTGRESQL
 
-CREATE TABLE PROPRIETARIOS(
-    NOME VARCHAR(20),
-    SOBRENOME VARCHAR(50),
-    ENDERECO TEXT
+CREATE TABLE proprietarios(
+    nome VARCHAR(20),
+    sobrenome VARCHAR(50),
+    endereco TEXT
+);
+
+CREATE TYPE TIPO_IMOVEL AS ENUM ('casa', 'apto', 'galpao');
+
+CREATE TABLE imoveis(
+    tipo_imovel TIPO_IMOVEL,
+    valor INTEGER,
+    observacoes TEXT,
+    detalhes TEXT,
+    endereco TEXT
 );
