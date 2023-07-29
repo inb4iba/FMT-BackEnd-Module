a3d994519c882908e1253c0eeea6c9aeb8dfb220
Icaro <iba1987@gmail.com>
2023-07-29 12:59:08 -0300

    exercicio 8

---
+++ Exercicios 8 - 9.sql
@@ -0,0 +1,31 @@
+-- Exercicio 8
+CREATE TABLE cargos (
+    id SERIAL PRIMARY KEY,
+    cargo VARCHAR(50)
+);
+
+CREATE TABLE funcionarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(80),
+    id_cargo INTEGER REFERENCES cargos(id)
+);
+
+CREATE TABLE telefones (
+    id SERIAL PRIMARY KEY,
+    telefone VARCHAR(20),
+    id_funcionario INTEGER REFERENCES funcionarios(id)
+);
+
+INSERT INTO cargos (cargo)
+VALUES ('Atendente'), ('Gerente');
+
+INSERT INTO funcionarios (nome, id_cargo)
+VALUES ('Marcos', 1), ('Maria', 2), ('Julia', 1);
+
+INSERT INTO telefones (telefone, id_funcionario)
+VALUES ('3654589', 1),
+('36545987', 1),
+('3654698', 2),
+('36524569', 2),
+('3654962', 3),
+('12365458', 3);
