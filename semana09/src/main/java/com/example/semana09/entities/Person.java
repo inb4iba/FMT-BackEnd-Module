2c214c1c2d82414b1d7f87862162f734d37506c2
Icaro <iba1987@gmail.com>
2023-07-31 18:13:52 -0300

    add primary key to entity

--- semana09/src/main/java/com/example/semana09/entities/Person.java
+++ semana09/src/main/java/com/example/semana09/entities/Person.java
@@ -1,11 +1,16 @@
 package com.example.semana09.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity(name = "persons")
 @Data
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
